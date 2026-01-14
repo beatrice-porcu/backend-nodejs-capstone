244ec039b42ea87c40214b9e1c5e9cb0f1dcbbe3
Beatrice Porcu <porcu@dauvea.it>
2026-01-14 08:48:21 +0100

    Fix comparison operator in loadData function for document length check

--- secondChance-backend/util/import-mongo/index.js
+++ secondChance-backend/util/import-mongo/index.js
@@ -4,6 +4,7 @@ const fs = require('fs');
 
 // MongoDB connection URL with authentication options
 let url = `${process.env.MONGO_URL}`;
+// eslint-disable-next-line no-path-concat
 let filename = `${__dirname}/secondChanceItems.json`;
 const dbName = 'secondChance';
 const collectionName = 'secondChanceItems';
@@ -28,7 +29,7 @@ async function loadData() {
         let cursor = await collection.find({});
         let documents = await cursor.toArray();
 
-        if(documents.length == 0) {
+        if (documents.length === 0) {
             // Insert data into the collection
             const insertResult = await collection.insertMany(data);
             console.log('Inserted documents:', insertResult.insertedCount);
