1cd104a6b697188009aa94c50aa7223938229fa5
bporcu <porcubeatrice21@gmail.com>
2026-01-15 05:20:13 -0500

    yml files

--- secondChance-backend/app.js
+++ secondChance-backend/app.js
@@ -20,7 +20,7 @@ app.use(express.json())
 
 // Route files
 const secondChanceRoutes = require('./routes/secondChanceItemsRoutes')
-const authRoutes = require('./routes/authRoutes')
+const authRoutes = require('./routes/auth')
 const searchRoutes = require('./routes/searchRoutes')
 const pinoHttp = require('pino-http')
 const logger = require('./logger')
